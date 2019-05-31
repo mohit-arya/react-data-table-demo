afa869ae37e31acb7fd292957e2897a230044db2
mohit-arya <mohitarya@outlook.com>
2019-06-01 00:49:39 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import DataTable from 'data-table';
+import { GET_POSTS_URL } from './constants';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    posts: [],
+    page: 1,
+    pages: 0
+  }
+
+  componentDidMount() {
+    this.fetchPosts();
+  }
+
+  async fetchPosts() {
+    const rawResponse = await fetch(GET_POSTS_URL);
+    const res = await rawResponse.json();
+    this.setState({
+      posts: res,
+      pages: Math.ceil(res.length / 10)
+    });
+  }
+
+  getColumns() {
+    const columns = [
+      {
+        header: "ID",
+        accessor: "id",
+        width: "5%",
+        cell: data => <strong>{data.id}</strong>,
+      },
+      {
+        header: "User ID",
+        width: "5%",
+        accessor: "userId"
+      },
+      {
+        header: "Title",
+        accessor: "title",
+        sortable: true
+      },
+      {
+        header: "Body",
+        accessor: "body",
+        sortable: true
+      }
+    ]
+    return columns;
+  }
+
+  getActions() {
+    return [
+      {
+        name: "Add",
+        className: "btn-primary",
+        onClick: () => console.log("Add Action Clicked")
+      },
+      {
+        name: "Export",
+        className: "btn-primary",
+        onClick: () => console.log("Export Action Clicked")
+      }
+    ];
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <DataTable
+          data={this.state.posts}
+          columns={this.getColumns()}
+          page={this.state.page - 1}
+          pages={this.state.pages}
+          defaultPageSize={10}
+          totalRows={this.state.posts.length}
+          manual={false}
+          onFetchData={this.manualFetchData}
+          className="table-bordered"
+          onCellClick={({ accessor, cellData, rowData }) => { console.log(accessor, cellData, rowData) }}
+          onRowClick={({ index, data }) => { console.log(index, data) }}
+          onSortChange={({ sortBy, order }) => console.log(sortBy, order)}
+          title="Table Title"
+          onSearch={(searchText) => console.log(searchText)}
+          actions={this.getActions()}
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
+
